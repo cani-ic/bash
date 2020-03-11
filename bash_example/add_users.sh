57c7f2e61e27fd00d4893592be79c2161aaf46ea
cani-ic <cani_ic@163.com>
2020-03-11 21:50:44 +0800

    add some comments

--- bash_example/add_users.sh
+++ bash_example/add_users.sh
@@ -6,19 +6,18 @@ USER_LIST=$(cat userlist.txt)
 
 read -p "Please Enter initial passwd for all users:" PASSWD
 
-for USER in $USER_LIST
+for USER in $USER_LIST	# note: gramma of -for- 
 do
-	id $USER &> /dev/null
+	id $USER &> /dev/null	#black hole in linux
 	if [ $? -eq 0 ]; then
 		echo "$USER already exists"
 	else 
 		useradd  $USER
-		echo "$PASSWD" | passwd --stdin $USER &>/dev/null
+		echo "$PASSWD" | passwd --stdin $USER &>/dev/null	#black hole in linux
 		if [ $? -eq 0 ]; then
 			echo "user $USRER add successfully."
 		else
 			echo "user $USRER add failed."
 		fi
 	fi
-
 done
